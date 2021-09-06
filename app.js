a9482ce75e715132b81b3bac7478ef8bd82c9237
Yasteel <64553959+Yasteel@users.noreply.github.com>
2021-09-06 02:57:10 +0200

    updates

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const goodMatch = require('yg-good-match');
 var fs = require('fs');
 var readme = fs.readFile('people.csv', 'utf8', function(err,data)
 {
-  var csvData = goodMatch.processCSV(data);
-  fs.writeFileSync('Output.txt', csvData);
-  console.log('Done!!');
+  if(err)
+  {
+    console.log('\nNo file Found, Please place csv file in Root Folder !!');
+  }
+  else
+  {
+    var csvData = goodMatch.processCSV(data);
+    fs.writeFileSync('Output.txt', csvData);
+    console.log('\nDone!\nView File "Output.txt" for results');
+  }
 });
